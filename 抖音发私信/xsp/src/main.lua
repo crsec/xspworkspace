775893793e999c2a61373c94058ea066478049e7
ouwen000 <wenhong520@qq.com>
2018-09-09 17:32:25 +0800

    last
    fix

--- 抖音发私信/xsp/src/main.lua
+++ 抖音发私信/xsp/src/main.lua
@@ -468,9 +468,122 @@ function send_follow_all()
     end
 end
 
+---
+
+function commnet_follow()
+	local TimeLine = mTime()
+	local OutTimes = 60*30*1000
+	local success = 0
+	local resttable = getadtext()
+	local last = false
+	local send_key = false
+	local bottom_key = 0
+	print_r(resttable)
+	
+	while mTime()-TimeLine < OutTimes do
+		if active(appbid,5)then
+			
+			logs('success->|'..success)
+			
+			if success <= tonumber(resttable['data']['mun']) then
+				if  d("抖音主界面") and tab("首页tab",true,3,6)then
+					if d("消息界面_粉丝",1,1)then
+						delay(2)
+					end
+					
+				elseif d("粉丝界面")then
+					if d("粉丝界面_关注",true,1)then
+						
+					elseif d('粉丝界面_暂时没有更多了')then
+						if bottom_key >= 5 then
+							return true
+						else
+							fensi = {83,263}
+							click(fensi[1],fensi[2]+bottom_key*200)
+							bottom_key = bottom_key + 1
+						end	
+						
+					elseif last then
+						moveTo(366,800,366,800-210,2,20)
+						delay(1)
+						last = false
+					elseif d("粉丝界面_相互关注")then
+						click(x-560,y)
+						last = true
+					end
+					
+				else
+					if d("抖音用户界面")then
+						if send_key then
+							t['消息界面_发过消息'].tap()
+							t['消息界面_发过消息'].tap()
+							send_key = false
+						elseif d("抖音用户界面_+关注",true,1)then
+						elseif d("抖音用户界面_发消息") or d('抖音用户界面_发消息_灰')then
+							t["抖音用户界面_发消息"].tap()
+						else
+							if d("抖音用户界面_发消息_位置")then
+								Dclick(x-400,y)
+								writePasteboard(resttable['data']['text']..zfb[rd(1,#zfb)]..","..zfb[rd(1,#zfb)])
+	--							input(getadtext()['data']['text']..zfb[rd(1,#zfb)]..","..zfb[rd(1,#zfb)])
+								if d("抖音用户界面_发消息粘贴",true)then
+									if d("抖音搜索界面_搜索按钮",true)then
+										last = true
+										success = success + 1
+										delay(rd(3,5))
+										t['消息界面_发过消息'].tap()
+										t['消息界面_发过消息'].tap()
+									end
+								end
+							end
+
+						--[[
+							if d("抖音用户界面_发消息_位置",true,1)then
+								if d("消息发送_失败")then
+									last = true
+									send_key = true
+								end
+							elseif d("消息界面_相册")then
+								t["消息界面_相册"].tap()
+								delay(1)
+								if d("图片_发送",true,1) then
+									send_key = true
+								end
+							else
+								t['消息界面_发过消息'].tap()
+							end
+						--]]
+						end
+					else
+						if d("qq登录按钮",true,1)then
+							delay(rd(8,10))
+							if d("qq登录按钮_登录",true,1)then
+								delay(rd(8,10))
+							end
+						elseif d("绑定手机跳过",true,1)then
+						elseif d("图片_发送",true,1)then
+							send_key = true
+						else
+							if d("抖音搜索界面")then
+								t['抖音搜索界面'].tap()
+							else
+								logs("otehr")
+								t['消息界面_发过消息'].tap()
+							end
+						end
+					end
+					
+				end
+			else
+				return true
+			end
+		end
+		delay(1)
+    end
+end
 
 
---[[]]
+--[[
 local lun_index = 0
 while 1 do
 	local resttable = getadtext()
@@ -496,10 +609,19 @@ while 1 do
 end
 --]]
 
+local ocr, msg = createOCR({
+    type = "tesseract", -- 指定tesseract引擎
+    path = "[external]", -- 使用开发助手/叉叉助手的扩展字库
+    lang = "chi_sim" -- 使用英文增强字库(注意需要提前下载好)
+})
 
+    code, text = ocr:getText({
+        rect = { 76,575,296,634 },
+        diff = {"0x5b5b5b-0x202020"}, -- 时间颜色为纯黑
+--        whitelist = "T0123456789" -- 添加所有可能出现的字符作为白名单
+    })
 
-
-
+logs(text)
 
 
 mSleep(2000) --延迟5秒
